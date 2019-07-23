587939c90711aec1873da33d3f4397753fdc5f93
Juan Pablo Ossa <ozjuanpa@gmail.com>
2019-07-23 11:02:22 -0500

    Added subset sum

--- talleres/taller02/taller02.py
+++ talleres/taller02/taller02.py
@@ -8,8 +8,20 @@ def gcd(p, q):
     return gcd(q, p%q)
 
 # suma grupo
-def sum_group(start, nums, target):
-    return 0
+def sum_group(nums, target):
+    if len(nums) == 1:
+        return nums[0] == target
+    sum = 0
+    for i in range(len(nums)):
+        sum = sum + nums[i]
+    sum_exists = sum == target
+    for i in range(len(nums)):
+        mutated = {}
+        for e in range(len(nums)):
+            if i != e:
+                mutated[len(mutated)] = nums[e]
+        sum_exists = sum_exists | sum_group(mutated, target)
+    return sum_exists
 
 # combinations
 def combinations(s):
@@ -29,10 +41,10 @@ test(gcd(12,36),12)
 test(gcd(8,12),4)
 test(gcd(72,108),36)
 test(gcd(162,270),54)
-test(sum_group(0, {2,4,8},9),False)
-test(sum_group(0, {2,4,8},8),True)
-test(sum_group(0, {10,2,2,5},9),True)
-test(sum_group(0, {10,2,2,5},17),True)
+test(sum_group([2,4,8],9),False)
+test(sum_group([2,4,8],8),True)
+test(sum_group([4,2,27000,5],9),True)
+test(sum_group([10,2,2,5],17),True)
 combinations("abc")
 combinations("Eafit")
 combinations("Hola")
