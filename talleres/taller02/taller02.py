b90b3c1313210aa0eb0e609220830bf5f19696ad
Juan Pablo Ossa <ozjuanpa@gmail.com>
2019-07-23 10:14:00 -0500

    Fix some things

--- talleres/taller02/taller02.py
+++ talleres/taller02/taller02.py
@@ -1,22 +1,25 @@
 
 # greatest common divider
-def gdc(p, q):
+def gcd(p, q):
     if p == 0:
         return q
     if q == 0:
         return p
     residue = p%q
-    return gdc(q, residue)
-    
+    return gcd(q, residue)
+
 
 # suma grupo
 def sum_group(start, nums, target):
+    return 0
 
 # combinations
 def combinations(s):
+    return 0
 
 # combinations auxiliary
 def _combinations_aux (prefix, s):
+    return 0
 
 
 # -- TESTS --
@@ -28,10 +31,10 @@ test(gcd(12,36),12)
 test(gcd(8,12),4)
 test(gcd(72,108),36)
 test(gcd(162,270),54)
-test(sum_group(0, {2,4,8},9),false)
-test(sum_group(0, {2,4,8},8),true)
-test(sum_group(0, {10,2,2,5},9),true)
-test(sum_group(0, {10,2,2,5},17),true)
+test(sum_group(0, {2,4,8},9),False)
+test(sum_group(0, {2,4,8},8),True)
+test(sum_group(0, {10,2,2,5},9),True)
+test(sum_group(0, {10,2,2,5},17),True)
 combinations("abc")
 combinations("Eafit")
 combinations("Hola")
