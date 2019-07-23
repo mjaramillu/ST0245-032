4f2526c7ac1fec862f0080bbcefc38142ff04aa0
Juan Pablo Ossa <ozjuanpa@gmail.com>
2019-07-23 09:49:38 -0500

    Add template and tests

---
+++ talleres/taller02/taller02.py
@@ -0,0 +1,27 @@
+
+# greatest common divider
+def gdc(p, q):
+
+# suma grupo
+def sum_group(start, nums, target):
+
+# combinations
+def combinations(s):
+
+# combinations auxiliary
+def _combinations_aux (prefix, s):
+
+
+# -- TESTS --
+
+def test(value, expected):
+    print("Value:", str(value), "Expected:", str(expected), "Passed:", str(value==expected))
+
+test(gcd(12,36),12)
+test(gcd(8,12),4)
+test(gcd(72,108),36)
+test(gcd(162,270),54)
+test(sum_group(0, {2,4,8},9),false)
+test(sum_group(0, {2,4,8},8),true)
+test(sum_group(0, {10,2,2,5},9),true)
+test(sum_group(0, {10,2,2,5},17),true)
