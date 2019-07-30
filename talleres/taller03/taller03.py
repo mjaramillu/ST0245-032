1202b7849332d8dda97b738c50b364e657e89418
Juan Pablo Ossa <ozjuanpa@gmail.com>
2019-07-30 09:59:31 -0500

    Added encryption stuff

--- talleres/taller03/taller03.py
+++ talleres/taller03/taller03.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
 
+import HES
+
+def decrypt_attempt(key):
+    result = HES.decrypt_from_file("encryptedFile", key)
+    if "42" in result:
+        return result
+    return False
+
 def hanoi(topN, a = "Tower1", b = "Tower2", c = "Tower3"):
 
 def subset(s, base = ""):
