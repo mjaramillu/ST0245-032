bc9243150dc49b867d19585404665620d34a13f2
Juan Pablo Ossa Zapata <ozjuanpa@gmail.com>
2019-08-02 22:38:35 -0500

    Improve lcs code

--- laboratorios/lab01/codigo/general.py
+++ laboratorios/lab01/codigo/general.py
@@ -1,10 +1,14 @@
 
 # Obtain the longest common subsequence for two strings of text
-def longest_common_subsequence(chain_a, chain_b):
-    if len(chain_a) == 0 or len(chain_b) == 0:
+def longest_common_subsequence(chain_a, chain_b, index_a = -2, index_b = -2):
+    if index_a == -2 or index_b == -2:
+        index_a = len(chain_a) - 1
+        index_b = len(chain_b) - 1
+    if index_a == -1 or index_b == -1:
         return 0
-    if chain_a[len(chain_a) -1] == chain_b[len(chain_b) -1]:
-        return longest_common_subsequence(chain_a[:-1], chain_b[:-1]) + 1
-    a = longest_common_subsequence(chain_a[:-1], chain_b)
-    b = longest_common_subsequence(chain_a, chain_b[:-1])
+    if chain_a[index_a] == chain_b[index_b]:
+        #print(len(chain_a), len(chain_b))
+        return longest_common_subsequence(chain_a, chain_b, index_a - 1, index_b - 1) + 1
+    a = longest_common_subsequence(chain_a, chain_b, index_a -1, index_b)
+    b = longest_common_subsequence(chain_a, chain_b, index_a, index_b - 1)
     return max(a,b)
--- laboratorios/lab01/codigo/test.py
+++ laboratorios/lab01/codigo/test.py
@@ -1,5 +1,42 @@
 import general
+import os
+import sys
 
-print(general.longest_common_subsequence("AXYT", "AYZX"))
-print(general.longest_common_subsequence("AGGGGGTAB", "GXTXAYB"))
-print(general.longest_common_subsequence("ABCDGH", "AEDFHR"))
+#TODO: CITATION: https://www.mkyong.com/python/python-how-to-list-all-files-in-a-directory/
+#TODO: CITATION: https://www.guru99.com/reading-and-writing-files-in-python.html
+
+def obtain_dna_files():
+    path = "dna_samples/"
+    files = []
+    for r, d, f in os.walk(path):
+        for file in f:
+            if '.dna' in file:
+                filepath = os.path.join(r, file)
+                f = open(filepath, "r")
+                this_file = {}
+                this_file["name"] = file
+                this_file["contents"] = f.read()
+                files.append(this_file)
+                f.close()
+    return files
+
+def compare_dna_files():
+    files = obtain_dna_files()
+    for i in range(len(files)):
+        for j in range(i,len(files)):
+            if i != j:
+                a = files[i]
+                b = files[j]
+                print("Comparing", a["name"], "with", b["name"], "...")
+                print("Size of A:", len(a["contents"])/1000.0, "KiB")
+                print("Size of B:", len(b["contents"])/1000.0, "KiB")
+                print("Longest common subsequence:", general.longest_common_subsequence(a["contents"], b["contents"]))
+
+def test(value, expected):
+    print("Value:", str(value), "Expected:", str(expected), "Passed:",  str(value==expected))
+
+print("Testing lcs...")
+test(general.longest_common_subsequence("AXYT", "AYZX"),2)
+test(general.longest_common_subsequence("AGGGGGTAB", "GXTXAYB"),4)
+test(general.longest_common_subsequence("ABCDGH", "AEDFHR"),3)
+compare_dna_files()
