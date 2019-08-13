5b780ec42d4386ec89535e8368eca97735ab72a8
Mauricio Jaramillo Uparela <mjaramillu@eafit.edu.co>
2019-08-13 11:55:32 -0500

    insertion_sort finish

--- talleres/taller05/taller05.py
+++ talleres/taller05/taller05.py
@@ -1,11 +1,14 @@
-def insertion_sort(arr, i=0):
-    if(i==len(arr)-1):
+def insertion_sort(arr, i=1):
+    if(i==len(arr)):
         return arr
-    if(arr[i+1]>=arr[i]):
+    elif(arr[i]>=arr[i-1]):
         return insertion_sort(arr, i+1)
-    else
-        a = arr[i]
-        arr[i]=arr[i+1]
-        arr[i+1]=a
-        return insertion_sort(arr, i)
+    else:
+        a = arr[i-1]
+        arr[i-1]=arr[i]
+        arr[i]=a
+        if (i>1):
+            return insertion_sort(arr, i-1)
+        else:
+            return insertion_sort(arr,i+1)
 print(insertion_sort([6,4,2,3,1,8,7]))
