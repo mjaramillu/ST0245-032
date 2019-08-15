a8ce6ba73e83f56887dfbf19d2464744b8a78f77
Juan Pablo Ossa <ozjuanpa@gmail.com>
2019-08-14 20:50:31 -0500

    Added complexity calculations

--- talleres/taller05/taller05.py
+++ talleres/taller05/taller05.py
@@ -1,13 +1,15 @@
-<<<<<<< HEAD
+
 from matplotlib import pyplot as pl
 import time
 import random
+import sys
 
+# Complejidad: O(n)
 def array_sum_loops (to_sum):
     result = 0 #C1
     for n in to_sum:
         result += n #C2 * to_sum
-    return result # T(to_sum) = C1 + C2 * to_sum = O(n) W
+    return result # T(to_sum) = C1 + C2 * to_sum = O(n)
 
 deltas = []
 ns = []
@@ -25,19 +27,40 @@ for i in range(20):
 
 pl.plot(ns, deltas)
 pl.show()
-=======
+
+# Complejidad: O(n^2)
 def insertion_sort(arr, i=1):
-    if(i==len(arr)):
+    if(i==len(arr)): # Not worse case
         return arr
-    elif(arr[i]>=arr[i-1]):
+    elif(arr[i]>=arr[i-1]): # T(n-1)
         return insertion_sort(arr, i+1)
     else:
-        a = arr[i-1]
-        arr[i-1]=arr[i]
-        arr[i]=a
-        if (i>1):
-            return insertion_sort(arr, i-1)
+        a = arr[i-1] #C1
+        arr[i-1]=arr[i] #C2
+        arr[i]=a #C3
+        if (i>1): #C4
+            return insertion_sort(arr, i-1) # T(n+1)
         else:
             return insertion_sort(arr,i+1)
-print(insertion_sort([9,76,8,5,0,2,9,1,7.2,-1,4,3]))
->>>>>>> master
+
+
+sys.setrecursionlimit(500000)
+
+
+deltas = []
+ns = []
+for i in range(20):
+    begin_time = time.time()
+    for w in range(100):
+        array_to_sum = []
+        for x in range(7 * (i+1)):
+            array_to_sum.append(random.randint(1,1000))
+        print(len(array_to_sum))
+        insertion_sort(array_to_sum)
+    end_time = time.time()
+    delta = end_time - begin_time
+    ns.append(len(array_to_sum))
+    deltas.append(delta)
+
+pl.plot(ns, deltas)
+pl.show()
