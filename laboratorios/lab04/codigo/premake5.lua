1396c373c0d0a6e803bba99b09a23f8fc021176a
Juan Pablo Ossa Zapata <ozjuanpa@gmail.com>
2019-10-05 22:02:26 -0500

    Add starter files for lab

---
+++ laboratorios/lab04/codigo/premake5.lua
@@ -0,0 +1,20 @@
+-- premake5.lua
+workspace "lab"
+   configurations { "Debug", "Release" }
+
+project "lab"
+   kind "ConsoleApp"
+   language "C"
+   targetdir "bin/%{cfg.buildcfg}"
+
+   links { }
+
+   files { "**.h", "**.c" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
